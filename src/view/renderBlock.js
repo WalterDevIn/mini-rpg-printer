a7b7775d1c3486ab6cc888ece41c4e4f7912739c
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 16:10:02 -0300

    Dispatch render for new block types

--- src/view/renderBlock.js
+++ src/view/renderBlock.js
@@ -1,11 +1,26 @@
 import { BLOCK_TYPES } from "../blocks/blockTypes.js";
 import { el } from "../shared/dom.js";
 import { frameToCss } from "../shared/geometry.js";
+import { renderGridBlock } from "./renderGridBlock.js";
+import { renderLineBlock } from "./renderLineBlock.js";
+import { renderRuledTextBlock } from "./renderRuledTextBlock.js";
 import { renderTextBlock } from "./renderTextBlock.js";
 
 export function renderBlock(args) {
   if (args.block.type === BLOCK_TYPES.text) {
     return renderTextBlock(args);
+  }
+
+  if (args.block.type === BLOCK_TYPES.line) {
+    return renderLineBlock(args);
+  }
+
+  if (args.block.type === BLOCK_TYPES.ruledText) {
+    return renderRuledTextBlock(args);
+  }
+
+  if (args.block.type === BLOCK_TYPES.gridBlock) {
+    return renderGridBlock(args);
   }
 
   return el("article", {
