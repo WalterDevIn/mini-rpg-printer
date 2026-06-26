5812750c75ee9b6514dfbb7f73f5c4bc3c1cd7cd
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 16:09:38 -0300

    Add grid block renderer

---
+++ src/view/renderGridBlock.js
@@ -0,0 +1,43 @@
+import { getCommonStyle, getInternalGridStyle, getRuledTextStyle } from "../blocks/blockStyle.js";
+import { isEditingBlock } from "../editor/editorSelectors.js";
+import { el } from "../shared/dom.js";
+import { createBlockElement, createEditableTextElement } from "./blockChrome.js";
+import { gridBackgroundToCss, ruledTextContainerStyleToCss, ruledTextStyleToCss } from "./blockStyleCss.js";
+
+export function renderGridBlock({ block, page, pageElement, editorState, controller }) {
+  const isEditing = isEditingBlock(editorState, block.id);
+  const commonStyle = getCommonStyle(block);
+  const ruledTextStyle = getRuledTextStyle(block);
+  const gridStyle = getInternalGridStyle(block);
+
+  const content = el("div", {
+    className: "block__content block__content--ruled-text block__content--grid-block",
+    style: {
+      ...ruledTextContainerStyleToCss(ruledTextStyle),
+      ...gridBackgroundToCss(gridStyle),
+    },
+    on: {
+      pointerdown: (event) => {
+        if (isEditing) event.stopPropagation();
+      },
+    },
+  }, [
+    createEditableTextElement({
+      block,
+      isEditing,
+      controller,
+      className: "block__text block__text--ruled",
+      style: ruledTextStyleToCss(ruledTextStyle),
+    }),
+  ]);
+
+  return createBlockElement({
+    block,
+    page,
+    pageElement,
+    editorState,
+    controller,
+    commonStyle,
+    children: [content],
+  });
+}
