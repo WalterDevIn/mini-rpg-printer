2474f9d1454a5f47e95c56579b6f7d299c7b0bec
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 17:03:08 -0300

    Separate block and page toolbar actions

--- src/view/renderToolbar.js
+++ src/view/renderToolbar.js
@@ -5,7 +5,7 @@ import { el, iconButton } from "../shared/dom.js";
 export function renderToolbar({ editorState, controller }) {
   return el("header", { className: "toolbar" }, [
     el("div", { className: "toolbar__title", textContent: "Mini RPG Printer" }),
-    el("div", { className: "toolbar__group" }, [
+    el("div", { className: "toolbar__group", title: "Agregar bloques" }, [
       iconButton({
         iconClass: "fa-solid fa-font",
         label: "Agregar texto libre",
@@ -32,21 +32,36 @@ export function renderToolbar({ editorState, controller }) {
         onClick: () => controller.addBlock(BLOCK_TYPES.image),
       }),
       iconButton({
+        iconClass: "fa-solid fa-icons",
+        label: "Agregar ícono Font Awesome",
+        onClick: () => controller.addBlock(BLOCK_TYPES.icon),
+      }),
+    ]),
+    el("div", { className: "toolbar__group", title: "Página y visualización" }, [
+      iconButton({
         iconClass: "fa-regular fa-clone",
         label: "Agregar par de hojas",
         onClick: () => controller.addSpread(),
-      }),
-      iconButton({
-        iconClass: "fa-regular fa-trash-can",
-        label: "Borrar bloque seleccionado",
-        disabled: !hasSelection(editorState),
-        onClick: () => controller.deleteSelectedBlock(),
       }),
       iconButton({
         iconClass: "fa-solid fa-table-cells",
         label: editorState.viewport.showGrid ? "Ocultar grilla" : "Mostrar grilla",
         active: editorState.viewport.showGrid,
         onClick: () => controller.toggleGrid(),
+      }),
+      iconButton({
+        iconClass: "fa-solid fa-book-open",
+        label: editorState.viewport.showPageMargin ? "Ocultar margen de anillos" : "Mostrar margen de anillos",
+        active: editorState.viewport.showPageMargin,
+        onClick: () => controller.togglePageMargin(),
+      }),
+    ]),
+    el("div", { className: "toolbar__group", title: "Selección" }, [
+      iconButton({
+        iconClass: "fa-regular fa-trash-can",
+        label: "Borrar bloque seleccionado",
+        disabled: !hasSelection(editorState),
+        onClick: () => controller.deleteSelectedBlock(),
       }),
     ]),
     el("div", { className: "toolbar__spacer" }),
