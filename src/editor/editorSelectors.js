961b6139dd11add3aa8d196a6bacf66d04b85156
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 17:39:02 -0300

    Support multi selection selectors

--- src/editor/editorSelectors.js
+++ src/editor/editorSelectors.js
@@ -1,14 +1,21 @@
-import { findBlockById } from "../document/documentQueries.js";
+import {
+  getPrimarySelectedBlock,
+  getSelectedBlockIds,
+  isBlockSelected,
+} from "./selectionHelpers.js";
 
 export function getSelectedBlock(editorState) {
-  if (!editorState.selection.blockId) return null;
-  return findBlockById(editorState.document, editorState.selection.blockId);
+  return getPrimarySelectedBlock(editorState);
 }
 
 export function isEditingBlock(editorState, blockId) {
   return editorState.interaction.editingBlockId === blockId;
 }
 
+export function isSelectedBlock(editorState, blockId) {
+  return isBlockSelected(editorState, blockId);
+}
+
 export function hasSelection(editorState) {
-  return Boolean(editorState.selection.blockId);
+  return getSelectedBlockIds(editorState).length > 0;
 }
