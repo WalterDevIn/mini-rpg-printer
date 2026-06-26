bacff4dc62cf8d5673d93fc5d6edceb43ee64890
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 15:32:50 -0300

    Preserve editor viewport scroll across renders

--- src/app/createEditorApp.js
+++ src/app/createEditorApp.js
@@ -2,14 +2,17 @@ import { createEditorController } from "../editor/editorController.js";
 import { createEditorState } from "../editor/editorState.js";
 import { installEditorEvents } from "../editor/installEditorEvents.js";
 import { renderEditor, renderEditorError } from "../view/renderEditor.js";
+import { withPreservedViewportScroll } from "./preserveScroll.js";
 
 export function createEditorApp({ rootElement }) {
   const editorState = createEditorState();
   let controller = null;
 
   function render() {
-    rootElement.innerHTML = "";
-    rootElement.appendChild(renderEditor({ editorState, controller }));
+    withPreservedViewportScroll(rootElement, () => {
+      rootElement.innerHTML = "";
+      rootElement.appendChild(renderEditor({ editorState, controller }));
+    });
   }
 
   controller = createEditorController({ editorState, render });
