9d9d1891a00b9f22853ad24b7085287484d03722
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 16:07:26 -0300

    Add line block definition

---
+++ src/blocks/lineBlockDefinition.js
@@ -0,0 +1,32 @@
+import { BLOCK_TYPES } from "./blockTypes.js";
+
+export const LINE_BLOCK_DEFAULT_PROPS = {
+  style: {
+    backgroundColor: "#111827",
+    fontFamily: "Arial",
+    fontSizePt: 11,
+    hasBorder: false,
+    borderRadiusMm: 0,
+    layer: 2,
+    bold: false,
+    italic: false,
+    strike: false,
+  },
+  line: {
+    angleDeg: 0,
+    thicknessMm: 0.5,
+  },
+};
+
+export const LINE_BLOCK_DEFINITION = {
+  type: BLOCK_TYPES.line,
+  label: "Línea",
+  iconClass: "fa-solid fa-minus",
+  defaultFrame: {
+    x: 10,
+    y: 10,
+    width: 40,
+    height: 5,
+  },
+  defaultProps: LINE_BLOCK_DEFAULT_PROPS,
+};
