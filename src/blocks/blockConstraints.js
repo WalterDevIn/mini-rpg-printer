f2cec188f57a7954a182391c6a90399de18bd7c2
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-27 12:02:23 -0300

    Read block constraints from registry

--- src/blocks/blockConstraints.js
+++ src/blocks/blockConstraints.js
@@ -1,11 +1,7 @@
-import { BLOCK_TYPES } from "./blockTypes.js";
+import { getBlockDefinition } from "./blockRegistry.js";
 
 const DEFAULT_MINIMUM_FRAME_SIZE = {};
 
-const MINIMUM_FRAME_SIZE_BY_TYPE = {
-  [BLOCK_TYPES.line]: { widthMm: 5, heightMm: 0.5 },
-};
-
 export function getMinimumFrameSize(block) {
-  return MINIMUM_FRAME_SIZE_BY_TYPE[block.type] ?? DEFAULT_MINIMUM_FRAME_SIZE;
+  return getBlockDefinition(block.type).constraints.minFrame ?? DEFAULT_MINIMUM_FRAME_SIZE;
 }
