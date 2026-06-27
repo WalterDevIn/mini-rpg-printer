99104d6ab4116f588dd0f4c2607ddf194d6ae500
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-27 13:25:47 -0300

    Resolve global colors for line blocks

--- src/view/renderLineBlock.js
+++ src/view/renderLineBlock.js
@@ -1,15 +1,17 @@
 import { getCommonStyle, getLineStyle } from "../blocks/blockStyle.js";
 import { el } from "../shared/dom.js";
 import { createBlockElement } from "./blockChrome.js";
+import { commonStyleToCss } from "./blockStyleCss.js";
 
 export function renderLineBlock({ block, page, pageElement, editorState, controller }) {
   const commonStyle = getCommonStyle(block);
+  const resolvedCommonCss = commonStyleToCss(commonStyle, editorState.globalColors);
   const lineStyle = getLineStyle(block);
   const line = el("div", {
     className: "block__line",
     style: {
       height: `${lineStyle.thicknessMm}mm`,
-      backgroundColor: commonStyle.backgroundColor,
+      backgroundColor: resolvedCommonCss.backgroundColor,
     },
   });
 
@@ -22,6 +24,8 @@ export function renderLineBlock({ block, page, pageElement, editorState, control
     commonStyle: {
       ...commonStyle,
       backgroundColor: "transparent",
+      backgroundOpacity: 0,
+      backgroundColorId: "",
       hasBorder: false,
     },
     style: {
