e36c038e070d23b67b1462dadb3e626bf3640178
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-29 20:49:28 -0300

    Track print preview panel state

--- src/editor/editorState.js
+++ src/editor/editorState.js
@@ -35,6 +35,7 @@ export function createEditorState() {
     ui: {
       sidebarCollapsed: false,
       collapsedPageIds: [],
+      printPreviewOpen: false,
     },
     activeTool: "select",
   };
